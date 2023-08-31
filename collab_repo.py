692be334bf6f92e5ee13d2027cbfe09b53531bae
Alekhya1422 <121009357+Alekhya1422@users.noreply.github.com>
2023-09-01 01:33:35 +1000

    Update collab_repo.py

--- collab_repo.py
+++ collab_repo.py
@@ -60,16 +60,14 @@ if st.button("Submit"):
         st.warning("Please fill in both your name and learning objectives.")
  #display ideas back to the user
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+
 st.divider()
 
 st.subheader('People with Similar Interests:')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+
 
 if st.session_state.ideasList:
     st.title("Ideas")
